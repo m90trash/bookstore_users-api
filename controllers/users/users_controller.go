a4275f805c2267001277a055c1a9abada9232ff2
mantas <m90trash@gmail.com>
2020-09-15 08:55:34 +0300

    domain

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,11 +2,21 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/m90trash/bookstore_users-api/domain/users"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+
+	var user users.User
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+
+	}
+
+	c.JSON(http.StatusCreated, &user)
+
 }
 
 func GetUser(c *gin.Context) {
--- domain/users/user.go
+++ domain/users/user.go
@@ -1,1 +1,8 @@
 package users
+
+type User struct {
+	Id         int    `json:"id"`
+	FullName   string `json:"full_name"`
+	Email      string `json:"email"`
+	CreateDate string `json:"create_date"`
+}
